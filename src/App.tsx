7eccf82f3192f3cecffe049e6d3d4b1568bf9ec6
sardarz <muhkween@gmail.com>
2023-05-26 18:22:03 +0300

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { Routes, Route, Outlet, Link } from "react-router-dom";
 import "./App.css";
 import { RequireAuth, useAuth } from "./auth/AuthProvider";
 import { LoginPage } from "./auth/LoginPage";
-import Layout from "./components/Layout";
-import styles from "./index.css"
+import Layout from "./components/Layout/Layout";
+import "./index.css"
+import Recruiter from "./components/Recruiter/Recruiter";
 
 const NotFound = () => {
   return <h1>empty</h1>;
@@ -12,20 +13,20 @@ const NotFound = () => {
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1> 
-      <h3 className="text-3xl">Basic example of react router dom</h3>
+    <div className="h-screen">
+      {/* <h3 className="text-4xl mb-12">Basic example of react router dom</h3> */}
       <Routes>
         <Route path="login" element={<LoginPage />} />
         <Route path="/" element={<Layout />}>
-          {/* <Route
+          <Route
             path="about"
             element={
               <RequireAuth>
+                <NotFound />
               </RequireAuth>
             }
-          /> */}
-
+          />
+          <Route path="test" element={<Recruiter />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
