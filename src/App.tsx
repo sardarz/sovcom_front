e1a9f99a8afd481a97623d295c85ce57c18a1af0
sardarz <muhkween@gmail.com>
2023-05-26 15:54:17 +0300

    add tailwind to project

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 import { RequireAuth, useAuth } from "./auth/AuthProvider";
 import { LoginPage } from "./auth/LoginPage";
 import Layout from "./components/Layout";
-import "./index.css"
+import styles from "./index.css"
 
 const NotFound = () => {
   return <h1>empty</h1>;
@@ -13,6 +13,7 @@ const NotFound = () => {
 function App() {
   return (
     <div>
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1> 
       <h3 className="text-3xl">Basic example of react router dom</h3>
       <Routes>
         <Route path="login" element={<LoginPage />} />
